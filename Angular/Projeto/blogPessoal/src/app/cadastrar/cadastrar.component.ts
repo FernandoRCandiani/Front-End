5d733511591b1ed217797d17164be8e055dde9cf
unknown <candianirfernando@gmail.com>
2021-05-20 10:11:37 -0300

    Implemetação dos alertas

--- Angular/Projeto/blogPessoal/src/app/cadastrar/cadastrar.component.ts
+++ Angular/Projeto/blogPessoal/src/app/cadastrar/cadastrar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Usuario } from '../model/Usuario';
+import { AlertasService } from '../service/alertas.service';
 import { AuthService } from '../service/auth.service';
 
 @Component({
@@ -16,7 +17,8 @@ export class CadastrarComponent implements OnInit {
 
   constructor(
     private authService: AuthService,
-    private router: Router
+    private router: Router,
+    private alertas: AlertasService
   ) { }
 
   ngOnInit() {
@@ -34,13 +36,32 @@ export class CadastrarComponent implements OnInit {
   cadastrar(){
     this.usuario.tipo = this.tipoUsuario
 
+    if(this.usuario.nome == ''){
+      this.alertas.showAlertDanger('Está faltando seu nome')
+    }
+
+    if(this.usuario.usuario == ''){
+      this.alertas.showAlertDanger('Está faltando seu usuario')
+    }
+
+    if(this.usuario.tipo == 'Selecione um tipo de usuário'){
+      this.alertas.showAlertInfo('Escolha uma tipo de usuario')
+    }
+
     if(this.usuario.senha != this.confirmarSenha){
-      alert('A senha está incorreta.')
+      this.alertas.showAlertDanger('A senha está incorreta.')
     } else {
       this.authService.cadastrar(this.usuario).subscribe((resp: Usuario) => {
         this.usuario = resp
         this.router.navigate(['/entrar'])
-        alert('Usuario cadastrado com sucesso!')
+        this.alertas.showAlertSuccess('Usuario cadastrado com sucesso!')
+      },erro =>{
+        if(erro.status == 500){
+          this.alertas.showAlertDanger('ERRO! Verificar dados cadastados.')
+        }
+        if(erro.status == 401){
+          this.alertas.showAlertDanger('Este usuario já é existente tente outro.')
+        }
       })
     }
 
--- Angular/Projeto/blogPessoal/src/app/edit/user-edit/user-edit.component.ts
+++ Angular/Projeto/blogPessoal/src/app/edit/user-edit/user-edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Usuario } from 'src/app/model/Usuario';
+import { AlertasService } from 'src/app/service/alertas.service';
 import { AuthService } from 'src/app/service/auth.service';
 import { environment } from 'src/environments/environment.prod';
 
@@ -19,7 +20,8 @@ export class UserEditComponent implements OnInit {
   constructor(
     private authService: AuthService,
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private alertas: AlertasService
   ) { }
 
   ngOnInit() {
@@ -46,12 +48,12 @@ export class UserEditComponent implements OnInit {
     this.usuario.tipo = this.tipoUsuario
 
     if(this.usuario.senha != this.confirmarSenha){
-      alert('A senha está incorreta.')
+      this.alertas.showAlertDanger('A senha está incorreta.')
     } else {
       this.authService.cadastrar(this.usuario).subscribe((resp: Usuario) => {
         this.usuario = resp
         this.router.navigate(['/inicio'])
-        alert('Usuario atualizado com sucesso, faça o login novamente.')
+        this.alertas.showAlertSuccess('Usuario atualizado com sucesso, faça o login novamente.')
         
         environment.token = ''
         environment.nome = ''
@@ -59,6 +61,11 @@ export class UserEditComponent implements OnInit {
         environment.id = 0
 
         this.router.navigate(['/entrar'])
+      },erro =>{
+        if(erro.status == 500){
+          this.alertas.showAlertDanger('ERRO!! Verificar mudanças')
+          this.alertas.showAlertInfo('Verifique tipo do usuario')
+        }
       })
     }
   }
